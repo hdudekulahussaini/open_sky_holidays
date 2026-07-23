688cb84690f8b4668c1b82590a617f9f2b8e18d9
Hussaini <hdudekulahussaini@gmail.com>
2026-07-23 15:46:30 +0530

    Refactor AdminMiddleware guest redirection and clean up web.php routes

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\TestimonialController;
 use App\Http\Controllers\Admin\TourController;
 use App\Http\Controllers\Admin\TourDetailController;
+use App\Http\Controllers\Admin\TourFeatureController;
 use App\Http\Controllers\Admin\TourTypeController;
 use App\Http\Controllers\Admin\TravelSupportSectionController;
 use App\Http\Controllers\Admin\WhatWeOfferController;
@@ -60,6 +61,13 @@ Route::prefix('admin')
         */
 
         Route::middleware(['auth', 'admin'])->group(function () {
+
+            /*
+            |--------------------------------------------------------------------------
+            | Dashboard
+            |--------------------------------------------------------------------------
+            */
+
             Route::get('/dashboard', [
                 DashboardController::class,
                 'index',
@@ -107,10 +115,15 @@ Route::prefix('admin')
                 'destroy',
             ])->name('enquiries.destroy');
 
+            /*
+            |--------------------------------------------------------------------------
+            | Resources
+            |--------------------------------------------------------------------------
+            */
+
             Route::resource('about-sections', AboutSectionController::class);
             Route::resource('travel-support', TravelSupportSectionController::class);
             Route::resource('why-choose-sections', WhyChooseSectionController::class);
-
             Route::resource('heroes', HeroController::class)->except('show');
             Route::resource('page-banners', PageBannerController::class)->except('show');
             Route::resource('categories', CategoryController::class)->except('show');
@@ -124,88 +137,20 @@ Route::prefix('admin')
             Route::resource('our-processes', OurProcessController::class);
             Route::resource('counters', CounterController::class);
             Route::resource('services', ServiceController::class);
-            Route::resource('what-we-offers', WhatWeOfferController::class);
-            Route::resource('about-our-core-values', AboutOurCoreValueController::class);
-            Route::resource('about-why-choose-us', AboutWhyChooseUsController::class);
+            Route::resource('what-we-offers', WhatWeOfferController::class)
+                ->parameters(['what-we-offers' => 'whatWeOffer'])
+                ->except('show');
+            Route::resource('about-our-core-values', AboutOurCoreValueController::class)
+                ->parameters(['about-our-core-values' => 'aboutOurCoreValue'])
+                ->except('show');
+            Route::resource('about-why-choose-us', AboutWhyChooseUsController::class)
+                ->parameters(['about-why-choose-us' => 'aboutWhyChooseUs'])
+                ->except('show');
             Route::resource('core-values', CoreValueController::class);
             Route::resource('tour-types', TourTypeController::class);
             Route::resource('tours', TourController::class);
             Route::resource('tour-details', TourDetailController::class);
-
-            /*
-            |--------------------------------------------------------------------------
-            | Offer Banners
-            |--------------------------------------------------------------------------
-            */
-
-            Route::resource(
-                'offer-banners',
-                OfferBannerController::class
-            )->except('show');
-
-            /*
-            |--------------------------------------------------------------------------
-            | Adventure Categories
-            |--------------------------------------------------------------------------
-            */
-
-            Route::resource(
-                'adventure-categories',
-                AdventureCategoryController::class
-            )->except('show');
-
-            /*
-            |--------------------------------------------------------------------------
-            | Adventures
-            |--------------------------------------------------------------------------
-            */
-
-            Route::resource(
-                'adventures',
-                AdventureController::class
-            )->except('show');
-
-            /*
-            |--------------------------------------------------------------------------
-            | What We Offer
-            |--------------------------------------------------------------------------
-            */
-
-            Route::resource(
-                'what-we-offers',
-                WhatWeOfferController::class
-            )
-                ->parameters([
-                    'what-we-offers' => 'whatWeOffer',
-                ])
-                ->except('show');
-            /*
-            |--------------------------------------------------------------------------
-            | About-Why-Choose-Us
-            |--------------------------------------------------------------------------
-            */
-
-            Route::resource(
-                'about-why-choose-us',
-                AboutWhyChooseUsController::class
-            )
-                ->parameters([
-                    'about-why-choose-us' => 'aboutWhyChooseUs',
-                ])
-                ->except('show');
-            /*
-            |--------------------------------------------------------------------------
-            | About-Our-Core-Values
-            |--------------------------------------------------------------------------
-            */
-            Route::resource(
-                'about-our-core-values',
-                AboutOurCoreValueController::class
-            )
-                ->parameters([
-                    'about-our-core-values' => 'aboutOurCoreValue',
-                ])
-                ->except('show');
+            Route::resource('tour-features', TourFeatureController::class);
 
             /*
             |--------------------------------------------------------------------------
